f73327b28e7459aef42231ea1408513c8e895136
Davron Ikhmatullaev <dikhmatullaev@gmail.com>
2025-01-22 18:36:35 +0500

    start

--- start.sh
+++ start.sh
@@ -6,21 +6,21 @@ export MKL_NUM_THREADS=4
 export CUDA_VISIBLE_DEVICES=0
 
 # Start Ollama in the background
-ollama serve &
-ollama pull bge-m3
-ollama pull llama3.1
+ollama serve --port 11434 &
+# ollama pull bge-m3
+# ollama pull llama3.1
 
 # Wait for Ollama to start up
 max_attempts=30
 attempt=0
-# while ! curl -s http://localhost:11434/api/tags >/dev/null; do
-#     sleep 1
-#     attempt=$((attempt + 1))
-#     if [ $attempt -eq $max_attempts ]; then
-#         echo "Ollama failed to start within 30 seconds. Exiting."
-#         exit 1
-#     fi
-# done
+while ! curl -s http://localhost:11434/api/tags >/dev/null; do
+    sleep 1
+    attempt=$((attempt + 1))
+    if [ $attempt -eq $max_attempts ]; then
+        echo "Ollama failed to start within 30 seconds. Exiting."
+        exit 1
+    fi
+done
 
 echo "Ollama is ready."
 
